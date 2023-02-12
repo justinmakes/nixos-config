c032e0b3573630a77ae3d37820e11f72e5be2af8
justinr <justin@worldcrafters.io>
2023-02-11 19:20:28 -0700

    set openssh.permitRootLogin = "no"

--- configuration.nix
+++ configuration.nix
@@ -163,7 +163,7 @@
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-  #services.openssh.permitRootLogin = "no";
+  services.openssh.permitRootLogin = "no";
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
